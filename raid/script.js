780e3538fc2ff850f86cd2a3e7e534a369a82455
tetrahydrant <138612905+tetrahydrant@users.noreply.github.com>
2023-07-04 14:42:14 -0600

    Create script.js

---
+++ raid/script.js
@@ -0,0 +1,18 @@
+//get data from the 🤟 StreamElements 🤟 data injection
+const name = '{{name}}';
+const amount = '{{amount}}';
+
+// vanilla es6 query selection (can use libraries and frameworks too)
+const userNameContainer = document.getElementById('username-container');
+const amountContainer = document.getElementById('amount-container');
+
+function getRandomInt() {
+    return "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  }
+
+userNameContainer.style.color = getRandomInt();
+amountContainer.style.color = getRandomInt();
+
+// change the inner html to animate it 🤪
+userNameContainer.innerHTML = name;
+amountContainer.innerHTML = amount;
