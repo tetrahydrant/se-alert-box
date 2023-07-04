5b4f35130acdc47e63618d7554619c67db81442e
tetrahydrant <138612905+tetrahydrant@users.noreply.github.com>
2023-07-04 13:54:43 -0600

    Update script.js

--- follower/script.js
+++ follower/script.js
@@ -1,7 +1,11 @@
+// Get the follower's username {{name}} using SE inline variable
+// https://dev.streamelements.com/docs/widgets/62d6299fe71df-custom-code-in-alertboxes#inline-variables-available
+
 const name = '{{name}}';
 const userNameContainer = document.getElementById("username-container")
 
 // function for random number to get HEX value
+// https://www.educative.io/answers/how-to-generate-a-random-color-in-javascript
 function getRandomInt() {
     return "#" + (Math.random()*0xFFFFFF<<0).toString(16);
   }
