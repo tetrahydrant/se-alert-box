2d52ef432857788331f2d2eb9603d3581f9b03ce
tetrahydrant <138612905+tetrahydrant@users.noreply.github.com>
2023-07-04 12:36:29 -0600

    Update script.js

--- follower/script.js
+++ follower/script.js
@@ -1,1 +1,17 @@
+//get data from the 🤟 StreamElements 🤟 data injection
+const name = '{{name}}';
+//const animation = 'wobble';
 
+// vanilla es6 query selection (can use libraries and frameworks too)
+//const userNameContainer = document.querySelector('#username-container');
+const userNameContainer = document.getElementById("username-container")
+
+// function for random number
+function getRandomInt() {
+    return "#" + (Math.random()*0xFFFFFF<<0).toString(16);
+  }
+
+userNameContainer.style.color = getRandomInt();
+
+// change the inner html to animate it 🤪
+userNameContainer.innerHTML = name;
